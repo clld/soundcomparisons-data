44641eb14dfcd301e86d659fe7886a687a5b64c7
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2018-06-28 11:23:35 +0200

    fixed addition --db-host as argument; default 'localhost'

--- pysoundcomparisons/db.py
+++ pysoundcomparisons/db.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 
 class DB(object):
     def __init__(self, host='localhost', db='soundcomparisons', user='soundcomparisons', password='pwd'):
-        self.engine = create_engine('mysql+pymysql://{1}:{2}@{4}/{3}?charset=utf8mb4'.format(
-            host, user, password, db))
+        self.engine = create_engine('mysql+pymysql://%s:%s@%s/%s?charset=utf8mb4' % (user, password, host, db))
 
     def __call__(self, *args, **kw):
         return self.engine.execute(*args, **kw)
