34e8722e570e5d2702adb152a69368d4f3c9a157
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2018-08-24 15:45:53 +0200

    extended MediaCatalog class by often required methods for querying info about cdstar sound files

--- pysoundcomparisons/mediacatalog.py
+++ pysoundcomparisons/mediacatalog.py
@@ -13,6 +13,8 @@ class MediaCatalog(object):
         'wav': 'audio/wav',
     }
 
+    __uid_map = None
+
     def __init__(self, repos):
         self.path = Path(repos).joinpath('soundfiles', 'catalog.json')
         if self.path.exists():
@@ -29,7 +31,24 @@ class MediaCatalog(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         jsonlib.dump(self.items, self.path, indent=4)
 
+    def __getitem__(self, key):
+        return self.items[key]
+
+    def __len__(self):
+        return len(self.items)
+
+    def __iter__(self):
+        for key in self.items:
+            yield key
+
+    def keys(self):
+        return list(self.items.keys())
+
+    def values(self):
+        return list(self.items.values())
+
     def add(self, api, d):
+        valid_extensions = ["." + x for x in self.__mimetypes.keys()]
         for sd in sorted(Path(d).iterdir(), key=lambda p: p.name):
             if sd.is_dir():
                 for word, files in groupby(
@@ -46,7 +65,8 @@ class MediaCatalog(object):
                         continue
                     print(word)
                     self.items[word] = self._add(
-                        api, word, [f for f in files if f.suffix in ['.wav', '.ogg', '.mp3']])
+                        api, word, [f for f in files if f.suffix in valid_extensions])
+                    self.__uid_map = None
 
     def _add(self, api, word, files):
         # create an object, add all files as bitstreams
@@ -67,3 +87,32 @@ class MediaCatalog(object):
             obj.delete()
             raise
         return obj.id, sorted(formats)
+
+    def file(self):
+        return self.path
+
+    def sound_extensions(self):
+        return sorted(self.__mimetypes.keys())
+
+    def keys_with_prefix(self, prefix="", trailing_delimiter="_"):
+        return [key for key in self.items if key.startswith(prefix + trailing_delimiter)]
+
+    def uid_for_key(self, item):
+        return self[item][0]
+
+    def key_for_uid(self, uid):
+        if self.__uid_map is None:
+            self.__uid_map = { self.items[x][0]: x for x in self.items }
+        return self.__uid_map[uid]
+
+    def uids(self):
+        if self.__uid_map is None:
+            self.__uid_map = { self.items[x][0]: x for x in self.items }
+        return self.__uid_map.keys()
+
+    def extensions_for_key(self, item):
+        try:
+            return self[item][1]
+        except:
+            return []
+
